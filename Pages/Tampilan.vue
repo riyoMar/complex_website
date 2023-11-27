f9eee6448c7a2a2d14e17ecc36aa385b84334d8d
YurannTaken <81124851+YurannTaken@users.noreply.github.com>
2023-11-27 17:03:02 +0700

    Add files via upload

---
+++ Pages/Tampilan.vue
@@ -0,0 +1,62 @@
+<script>
+
+</script>
+
+<script setup>
+
+</script>
+
+<template>
+    <div class="container">
+        <table class="table table-dark">
+  <thead>
+    <th scope="row" colspan="5" style="text-align: center; font-size: larger;">Edit Picture</th>
+  </thead>
+  <tbody>
+    <tr>
+        <td style="font-size: large;">image.jpg  </td>
+        <td style="text-align: right;"><button type="button" class="btn btn-secondary" style="font-size: large;">Edit</button></td>
+    </tr>
+  </tbody>
+</table>
+    </div>
+
+    <div class="container">
+        <table class="table table-dark">
+  <thead>
+    <th scope="row" colspan="5" style="text-align: center; font-size: larger;">Edit Text</th>
+  </thead>
+  <tbody>
+    <tr class="table-active">
+      <td colspan="4" style="font-size: larger;">Interlude</td>
+
+    </tr>
+    <tr>
+        <td colspan="5" style="font-size: large;;">Text Here aaaaaaaaaaaaaa</td>
+    </tr>
+    <td colspan="5" style="text-align: right;"><div><button type="button" class="btn btn-secondary" style="font-size: large;">Edit</button>  <button type="button" class="btn btn-secondary" style="font-size: large;">Hapus</button></div></td>
+
+    <tr class="table-active">
+      <td colspan="5" style="font-size: larger;">Introduction</td>
+
+    </tr>
+    <tr>
+        <td colspan="5" style="font-size: large;;">Text Here aaaaaaaaaaaaaa</td>
+    </tr>
+    <tr>
+        <td colspan="5" style="text-align: right;"><div><button type="button" class="btn btn-secondary" style="font-size: large; text-align: right;">Edit</button>  <button type="button" class="btn btn-secondary" style="font-size: large;">Hapus</button></div></td>
+    </tr>
+  </tbody>
+  <tfoot>
+    <tr>
+        <td colspan="5" style="text-align: center;"><button type="button" class="btn btn-secondary" style="font-size: large;">Submit</button></td>
+    </tr>
+  </tfoot>
+</table>
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
